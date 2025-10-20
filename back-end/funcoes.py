26bbfcde25b7cb47307f8502cf05a8c0203e93a6
Davi Tomé <Daviptome2008@gmail.com>
2025-10-20 10:39:46 -0300

    Função Inserir Produtos

--- back-end/funcoes.py
+++ back-end/funcoes.py
@@ -20,3 +20,20 @@ def criar_tabela():
             conexao.close()   
 
 #criar_tabela()
+
+def inserir_produto(nome, categoria, preco, quantidade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+            "INSERT INTO produtos (nome, categoria, preco, quantidade) VALUES(%s, %s, %s, %s)",
+            (nome, categoria, preco, quantidade)
+            )
+            conexao.commit()
+        except Exception as erro:
+            print(f"Erro ao inserir produto: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+#inserir_produto("", "", , )
